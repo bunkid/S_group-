9a9e919eb10a4df47e7757df5a689b2558d49b7b
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2022-12-29 23:28:26 +0700

    60%

--- landing_page/public/script/script.js
+++ landing_page/public/script/script.js
@@ -1,17 +1,36 @@
 
 
-var coll = document.getElementsByClassName("card");
-var i;
+// var coll = document.getElementsByClassName("card");
+// var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.firstElementChild;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-      content.style.transition= "all 2.5 ease-in";
-    }
-  });
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     var active = document.getElementsByClassName("card--active")
+//     if(active) {
+//         this.classList.remove("card--active");
+//     }
+//     else {
+//         this.classList.add("card--active");
+//     }
+//   });
+// }
+var coll = document.getElementsByClassName("card");
+function hidecard()
+{
+   this.classList.remove("card--active");
 }
+function opencard()
+{
+   this.classList.add("card--active");
+}
+    for( i = 0; i <coll.length; i++)
+    {
+       if(coll[i].classList.contains("card--active"))
+       {
+           coll[i].addEventListener("click",hidecard);
+       }
+       else
+       {
+        coll[i].addEventListener("click",opencard);
+       }
+    }
