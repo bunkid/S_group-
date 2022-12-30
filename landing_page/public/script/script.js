811f4d5b3e99828f4d8cbe2af4a96156a8ca38be
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2022-12-30 16:46:16 +0700

    lession 8 js excise

--- landing_page/public/script/script.js
+++ landing_page/public/script/script.js
@@ -1,36 +1,50 @@
 
-
-// var coll = document.getElementsByClassName("card");
-// var i;
-
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     var active = document.getElementsByClassName("card--active")
-//     if(active) {
-//         this.classList.remove("card--active");
-//     }
-//     else {
-//         this.classList.add("card--active");
-//     }
-//   });
-// }
 var coll = document.getElementsByClassName("card");
-function hidecard()
+
+function removeall()
 {
-   this.classList.remove("card--active");
-}
-function opencard()
-{
-   this.classList.add("card--active");
-}
     for( i = 0; i <coll.length; i++)
     {
-       if(coll[i].classList.contains("card--active"))
-       {
-           coll[i].addEventListener("click",hidecard);
-       }
-       else
-       {
-        coll[i].addEventListener("click",opencard);
-       }
+        if(  coll[i].classList.contains("card--active") )
+        {
+            coll[i].classList.remove("card--active");
+        }
     }
+}
+    
+for( i = 0; i <coll.length; i++)
+    { 
+      
+      coll[i].addEventListener("click",function()
+    {
+
+        
+              if(  this.classList.contains("card--active") )
+              {
+                this.classList.remove("card--active");
+    
+            }
+            else
+            {
+                removeall();
+                this.classList.add("card--active");
+            }
+            
+        
+    })
+    }
+
+    // for(const btn of coll)
+    // {
+    //     btn.addEventListener("click",function()
+    //     {
+    //         if(btn.classList.contains("card--active"))
+    //         {   
+    //             btn.classList.remove("card--active");
+    //         }else
+    //         {             
+    //             btn.classList.add("card--active");
+    //         }
+    //     })
+    // }
+
